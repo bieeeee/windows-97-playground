c00fbbd3ac1fa271f457ce403cc12ca15af3f64f
bieeeee <janviertrois@naver.com>
2024-03-11 16:01:25 +0900

    [feat] Define interfaces

---
+++ @types/Project.d.ts
@@ -0,0 +1,11 @@
+interface Project {
+    id: number;
+    team: number;
+    title: string;
+    period: string;
+    desc: string;
+    role: string[];
+    stack: string[];
+    image: string;
+    repo: string;
+}
---
+++ @types/Skills.d.ts
@@ -0,0 +1,3 @@
+interface Skills {
+    [key: string]: number;
+}
