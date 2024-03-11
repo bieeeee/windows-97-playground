6543206be76f1b4739718ab005825cdb7f98a127
bieeeee <janviertrois@naver.com>
2024-03-11 17:50:51 +0900

    [feat] Add icon images

---
+++ public/assets/images/index.ts
@@ -0,0 +1,11 @@
+import logo from "./images/logo-icon.png";
+import prompt from './images/prompt.png';
+import close from './images/close.png';
+import folder from './images/folder.png';
+import openFolder from './images/folder-open.png';
+import linkedin from './images/linkedin.png';
+import github from './images/github.png';
+import calculator from './images/calculator.png';
+import velog from './images/velog.png';
+
+export { logo, prompt, close, folder, openFolder, linkedin, github, calculator, velog }
