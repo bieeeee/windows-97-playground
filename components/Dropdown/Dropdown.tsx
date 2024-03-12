e1ac5972f784bd4981cfe19c08ab7f5e72e8e379
bieeeee <janviertrois@naver.com>
2024-03-12 11:04:25 +0900

    [fix] Change position, hover effect

--- components/Dropdown/Dropdown.tsx
+++ components/Dropdown/Dropdown.tsx
@@ -1,7 +1,7 @@
 'use client'
+import Link from 'next/link';
 import { useState } from 'react';
 import { MenuItems } from './MenuItem';
-import Link from 'next/link';
 
 function Dropdown() {
   const [click, setClick] = useState(false);
@@ -10,13 +10,13 @@ function Dropdown() {
   return (
     <ul
       onClick={handleClick}
-      className={!click ? 'cBorder w-[12.5rem] m-0 p-0 absolute top-[2.375rem] left-[.1875rem] text-start z-0' : 'hidden'}
+      className={!click ? 'cBorder w-[12.5rem] m-0 p-0 absolute top-6 left-1 text-start z-0' : 'hidden'}
     >
       {MenuItems.map((item, index) => {
         return (
           <Link href={item.path} key={index} replace>
-            <li className={'bg-gray-200 hover:bg-dark-navy hover:text-white'} onClick={() => setClick(false)}>
-              <span className={'block w-full h-full p-4 text-base hover:text-white'}>
+            <li className={'bg-gray-200 hover:bg-blue-300'} onClick={() => setClick(false)}>
+              <span className={'block w-full h-full p-4 hover:text-white'}>
                 {item.title}
               </span>
             </li>
