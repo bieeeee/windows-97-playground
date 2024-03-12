7c8ec4154e0fdcdec0e9dc98af48e37d61585414
bieeeee <janviertrois@naver.com>
2024-03-12 11:09:16 +0900

    [fix] Remove props, adjust padding

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,9 +4,7 @@ import { useState } from 'react';
 import Dropdown from '@components/Dropdown/Dropdown';
 import { logo } from '@images';
 
-type Props = {}
-
-const Navbar: React.FC = (props: Props) => {
+const Navbar: React.FC = () => {
     const [dropdown, setDropdown] = useState(false);
 
     const onMouseEnter = () => {
@@ -22,14 +20,14 @@ const Navbar: React.FC = (props: Props) => {
     };
 
     return (
-        <nav className={'w-full h-10 p-3 flex items-center cBorder sticky z-10 top-0'}>
+        <nav className={'w-full h-11 py-3 px-1 flex items-center cBorder sticky z-10 top-0'}>
             <div
                 className={'h-full flex items-center relative'}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
                 onClick={onClick}
             >
-                <div className={'p-1 flex gap-1.5 items-center justify-center cBorder cursor-pointer'}>
+                <div className={'px-1 py-1.5 flex gap-1.5 items-center justify-center cBorder text-sm cursor-pointer'+ (dropdown? ' active': '')}>
                     <Image src={logo} alt="logo" width={19} height={19} priority={true} />
                     <span>Start</span>
                 </div>
